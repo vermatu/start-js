74f6904cf8650573250610ed1a2b6b2212934278
vermatu <138736607+vermatu@users.noreply.github.com>
2024-04-24 06:17:01 +0000

    let const and Var

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+ const accountId = "76272"
+ let accountName = "Tushar Verma"
+ let accountType = "Zero balance"
+ let accountBalance = "1 cr"
+ console.table([accountId, accountName , accountType , accountBalance ])
+ /*
+ we dont use var keyword anymore because of block scope and functional scope
+ const keyword initialise value of a variable constant so that it cannot be changed
+ 
+ */
